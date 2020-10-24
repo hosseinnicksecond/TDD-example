78d595631306eb9d0d138e41056703914bcf933e
hossein nick <hossinnick62@gmail.com>
2020-10-24 20:01:06 +0330

    create .gitignore

---
+++ src/test/java/home/train/TDD/TddApplicationTests.java
@@ -0,0 +1,13 @@
+package home.train.TDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
